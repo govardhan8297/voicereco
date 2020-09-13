02c887d91c7417bdadf777f8fc5d9a5a8160bf0f
Naga Govardhan <66250220+govardhan8297@users.noreply.github.com>
2020-09-13 06:22:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector(".talk");
 const content = document.querySelector(".content");
+var date = new Date();
+const hours = date.getHours;
+const minutes = date.getMinutes;
+const seconds = date.getSeconds;
 
 const greetings = [
   "Im good, how are you",
@@ -12,6 +16,22 @@ const weather = [
   "Its gonna rain today",
   "Its a nice day for a trip",
 ];
+
+const time = [hours + "" + minutes + "" + seconds];
+
+const introduce = [
+  "Im a Person that was designed to meet your needs",
+  "Im an AI",
+  "Im your shadow",
+];
+
+const google = [
+  "Im not google just a similar type of that, you can call me naga",
+  "At your service",
+  "Present sir",
+];
+
+const greetings2 = ["hai", "hai, how are you doing today", "hello there"];
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -42,6 +62,23 @@ function readOutLoud(message) {
   }
   if (message.includes("how is the weather")) {
     const finalText = weather[Math.floor(Math.random() * weather.length)];
+    speech.text = finalText;
+  }
+  if (message.includes("who are you")) {
+    const finalText = introduce[Math.floor(Math.random() * introduce.length)];
+    speech.text = finalText;
+  }
+  if (message.includes("hai")) {
+    const finalText = greetings2[Math.floor(Math.random() * greetings2.length)];
+    speech.text = finalText;
+  }
+  if (message.includes("Google")) {
+    const finalText = google[Math.floor(Math.random() * google.length)];
+    speech.text = finalText;
+  }
+
+  if (message.includes("time")) {
+    const finalText = time[Math.floor(Math.random() * time.length)];
     speech.text = finalText;
   }
 
